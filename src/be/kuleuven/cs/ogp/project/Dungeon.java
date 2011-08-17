a36cdda4565427dcee2361802ef8537e79cc4225
Frederic Hannes <freddy1990@gmail.com>
2011-08-17 10:22:29 +0200

    Finished the implementation of dimensions.

--- src/be/kuleuven/cs/ogp/project/Dungeon.java
+++ src/be/kuleuven/cs/ogp/project/Dungeon.java
@@ -1,7 +1,9 @@
 package be.kuleuven.cs.ogp.project;
 
 import be.kuleuven.cs.ogp.project.tools.Point3D;
+import be.kuleuven.cs.som.annotate.Basic;
 import be.kuleuven.cs.som.annotate.Model;
+import be.kuleuven.cs.som.annotate.Raw;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +11,14 @@ import java.util.Map;
 /**
  * The class containing the most basic dungeon type.
  *
- * @author Frederic Hannes
+ * @invar   The X dimension of the dungeon can never be larger than the maximum allowed X dimension.
+ *          | this.getXDim() <= this.getXDimMax()
+ * @invar   The Y dimension of the dungeon can never be larger than the maximum allowed Y dimension.
+ *          | this.getYDim() <= this.getYDimMax()
+ * @invar   The Z dimension of the dungeon can never be larger than the maximum allowed Z dimension.
+ *          | this.getZDim() <= this.getZDimMax()
+ *
+ * @author  Frederic Hannes
  */
 public class Dungeon {
 
@@ -44,8 +53,35 @@ public class Dungeon {
     private long zDim;
 
     /**
+     * The map containing all of the squares in the dungeon.
+     */
+    private Map<Point3D, Square> squares = new HashMap<>();
+
+    /**
+     * Creates a new instance of dungeon.
+     *
+     * @effect  Sets the maximum dungeon dimensions to maximum value for Long.
+     *          | setXDimMax(Long.MAX_VALUE)
+     *          | setYDimMax(Long.MAX_VALUE)
+     *          | setZDimMax(Long.MAX_VALUE)
+     * @effect  Sets the current dungeon dimensions to 0.
+     *          | setXDim(0)
+     *          | setYDim(0)
+     *          | setZDim(0)
+     */
+    public Dungeon() {
+        setXDimMax(Long.MAX_VALUE);
+        setYDimMax(Long.MAX_VALUE);
+        setZDimMax(Long.MAX_VALUE);
+        setXDim(0);
+        setYDim(0);
+        setZDim(0);
+    }
+
+    /**
      * Returns the maximum size for the X dimension.
      */
+    @Basic @Raw
     public long getXDimMax() {
         return xDimMax;
     }
@@ -58,7 +94,7 @@ public class Dungeon {
      * @post    The new maximum dimension equals the given maximum dimension.
      *          | new.getXDimMax() == xDimMax
      */
-    @Model
+    @Model @Raw
     protected void setXDimMax(long xDimMax) {
         this.xDimMax = xDimMax;
     }
@@ -66,6 +102,7 @@ public class Dungeon {
     /**
      * Returns the maximum size for the Y dimension.
      */
+    @Basic @Raw
     public long getYDimMax() {
         return yDimMax;
     }
@@ -78,7 +115,7 @@ public class Dungeon {
      * @post    The new maximum dimension equals the given maximum dimension.
      *          | new.getYDimMax() == yDimMax
      */
-    @Model
+    @Model @Raw
     protected void setYDimMax(long yDimMax) {
         this.yDimMax = yDimMax;
     }
@@ -86,6 +123,7 @@ public class Dungeon {
     /**
      * Returns the maximum size for the Z dimension.
      */
+    @Basic @Raw
     public long getZDimMax() {
         return zDimMax;
     }
@@ -98,14 +136,110 @@ public class Dungeon {
      * @post    The new maximum dimension equals the given maximum dimension.
      *          | new.getZDimMax() == zDimMax
      */
-    @Model
+    @Model @Raw
     protected void setZDimMax(long zDimMax) {
         this.zDimMax = zDimMax;
     }
 
     /**
-     * The map containing all of the squares in the dungeon.
+     * Returns the size of the X dimension of the dungeon.
      */
-    private Map<Point3D, Square> squares = new HashMap<>();
+    @Basic @Raw
+    public long getXDim() {
+        return xDim;
+    }
+
+    /**
+     * Sets the current X-dimension size.
+     *
+     * @param   xDim
+     *          The given size.
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is smaller than the old one.
+     *          | xDim < this.getXDim()
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is larger than the maximum allowed size.
+     *          | xDim > this.getXDimMax()
+     */
+    @Model @Raw
+    private void setXDim(long xDim) throws IllegalArgumentException {
+        if (xDim < this.getXDim())
+            throw new IllegalArgumentException("The new X dimension has to be larger than the old one!");
+        if (xDim > this.getXDimMax())
+            throw new IllegalArgumentException("The new X dimension is larger than the maximum allowed size!");
+        this.xDim = xDim;
+    }
+
+    /**
+     * Returns the size of the Y dimension of the dungeon.
+     */
+    @Basic @Raw
+    public long getYDim() {
+        return yDim;
+    }
+
+    /**
+     * Sets the current Y-dimension size.
+     *
+     * @param   yDim
+     *          The given size.
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is smaller than the old one.
+     *          | yDim < this.getYDim()
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is larger than the maximum allowed size.
+     *          | yDim > this.getYDimMax()
+     */
+    @Model @Raw
+    public void setYDim(long yDim) throws IllegalArgumentException {
+        if (yDim < this.getYDim())
+            throw new IllegalArgumentException("The new Y dimension has to be larger than the old one!");
+        if (yDim > this.getYDimMax())
+            throw new IllegalArgumentException("The new Y dimension is larger than the maximum allowed size!");
+        this.yDim = yDim;
+    }
+
+    /**
+     * Returns the size of the Z dimension of the dungeon.
+     */
+    @Basic @Raw
+    public long getZDim() {
+        return zDim;
+    }
+
+    /**
+     * Sets the current Z-dimension size.
+     *
+     * @param   zDim
+     *          The given size.
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is smaller than the old one.
+     *          | zDim < this.getZDim()
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the new dimension is larger than the maximum allowed size.
+     *          | zDim > this.getZDimMax()
+     */
+    @Model @Raw
+    public void setZDim(long zDim) throws IllegalArgumentException {
+        if (zDim < this.getZDim())
+            throw new IllegalArgumentException("The new Z dimension has to be larger than the old one!");
+        if (zDim > this.getZDimMax())
+            throw new IllegalArgumentException("The new Z dimension is larger than the maximum allowed size!");
+        this.zDim = zDim;
+    }
+
+    /**
+     * Adds a square to the dungeon.
+     *
+     * @param   square
+     * @param   pos
+     * @throws  IllegalArgumentException
+     *          Throws an illegal argument exception if the given square is invalid.
+     */
+    public void addSquare(Square square, Point3D pos) throws IllegalArgumentException {
+        if (square == null)
+            throw new IllegalArgumentException("Invalid square!");
+        // TODO: Finish
+    }
 
 }
--- src/be/kuleuven/cs/ogp/project/tools/Point3D.java
+++ src/be/kuleuven/cs/ogp/project/tools/Point3D.java
@@ -9,9 +9,9 @@ import be.kuleuven.cs.som.annotate.Basic;
  */
 public class Point3D {
 
-	private int x;
-	private int y;
-	private int z;
+	private long x;
+	private long y;
+	private long z;
 
 	/**
 	 * Initializes this 3 dimensional point with given x, y and z values.
@@ -23,7 +23,7 @@ public class Point3D {
 	 * @effect	The given z-coordinate is set as the new z-coordinate.
 	 * 			| setZ(z)
 	 */
-	public Point3D(int x, int y, int z) {
+	public Point3D(long x, long y, long z) {
 		setX(x);
 		setY(y);
 		setZ(z);
@@ -33,7 +33,7 @@ public class Point3D {
 	 * Return the x-coordinate stored in the object.
 	 */
 	@Basic
-	public int getX() {
+	public long getX() {
 		return x;
 	}
 
@@ -46,7 +46,7 @@ public class Point3D {
 	 * 			| new.getX() == x
 	 */
 	@Basic
-	public void setX(int x) {
+	public void setX(long x) {
 		this.x = x;
 	}
 
@@ -54,7 +54,7 @@ public class Point3D {
 	 * Return the y-coordinate stored in the object.
 	 */
 	@Basic
-	public int getY() {
+	public long getY() {
 		return y;
 	}
 
@@ -67,7 +67,7 @@ public class Point3D {
 	 * 			| new.getY() == y
 	 */
 	@Basic
-	public void setY(int y) {
+	public void setY(long y) {
 		this.y = y;
 	}
 
@@ -75,7 +75,7 @@ public class Point3D {
 	 * Return the z-coordinate stored in the object.
 	 */
 	@Basic
-	public int getZ() {
+	public long getZ() {
 		return z;
 	}
 
@@ -88,7 +88,7 @@ public class Point3D {
 	 * 			| new.getZ() == z
 	 */
 	@Basic
-	public void setZ(int z) {
+	public void setZ(long z) {
 		this.z = z;
 	}
 
